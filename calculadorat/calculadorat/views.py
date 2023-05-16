f038af11c5c67bfa3e7c5c7eb1b7003e1a23a21f
DamarisCaballero <damariscallero011@gmail.com>
2023-05-15 20:51:41 -0600

    Mensaje de commit

--- calculadorat/calculadorat/views.py
+++ calculadorat/calculadorat/views.py
@@ -29,8 +29,23 @@ def calculadora(request):
 
 def lista_usuarios(request):
     usuarios = [
-        {'nombre': 'Juan', 'edad': 25},
-        {'nombre': 'María', 'edad': 30},
-        {'nombre': 'Ana', 'edad': 20},
-    ]
+    {'nombre': 'Carlos Eduardo Mata', 'edad': 33},
+    {'nombre': 'Sofía Ama Paz', 'edad': 27},
+    {'nombre': 'Alejandro Jose de Pez', 'edad': 19},
+    {'nombre': 'María Fernanda López', 'edad': 29},
+    {'nombre': 'Juan Carlos Ramírez', 'edad': 23},
+    {'nombre': 'Ana Sofía Rodríguez', 'edad': 32},
+    {'nombre': 'Pedro Alejandro Gómez', 'edad': 21},
+    {'nombre': 'Amelia Gabriela Pérez', 'edad': 27},
+    {'nombre': 'Andres Miguel Sánchez', 'edad': 25},
+    {'nombre': 'Carolina Elizabeth Torres', 'edad': 30},
+    {'nombre': 'Ricardo Antonio Mendoza', 'edad': 28},
+    {'nombre': 'Isabel Cristina Morales', 'edad': 29},
+    {'nombre': 'Alejandra Cristina Morales', 'edad': 39},
+]
+    for usuario in usuarios:
+        if usuario['nombre'].startswith('A'):
+            usuario['negrita'] = True
+        else:
+            usuario['negrita'] = False
     return render(request, 'lista-usuarios.html', {'usuarios': usuarios})
